a9139dfb3ae9ac2f9ffb9d2778e15ac18108f459
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-05-23 09:44:40 +0200

    moved

--- index_calculator/_tables.py
+++ index_calculator/_tables.py
@@ -8,7 +8,7 @@ def read_from_json(jsfile):
         return json.load(f)
 
 
-ijson = read_from_json("../tables/indices.json")
-xjson = read_from_json("../tables/xcalc.json")
-pjson = read_from_json("../tables/projects.json")
-istjson = read_from_json("../tables/institutions.json")
+ijson = read_from_json("tables/indices.json")
+xjson = read_from_json("tables/xcalc.json")
+pjson = read_from_json("tables/projects.json")
+istjson = read_from_json("tables/institutions.json")
