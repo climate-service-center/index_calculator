a6fea3ac97aecc5e1680e41454fb1d0de4678bc4
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-05-23 13:50:14 +0200

    List with required input variables

--- index_calculator/__init__.py
+++ index_calculator/__init__.py
@@ -9,6 +9,7 @@ from ._postprocessing import PostProcessing as postprocessing  # noqa
 from ._preprocessing import PreProcessing as preprocessing  # noqa
 from ._processing import Processing as processing  # noqa
 from ._tables import ijson  # noqa
+from ._tables import inpjson  # noqa
 from ._tables import istjson  # noqa
 from ._tables import pjson  # noqa
 from ._tables import xjson  # noqa
