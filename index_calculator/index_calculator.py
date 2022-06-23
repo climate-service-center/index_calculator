ea5e14b9f96bed11921f2b175315134e88b44efb
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-06-23 10:05:51 +0200

    Add docstings

--- index_calculator/index_calculator.py
+++ index_calculator/index_calculator.py
@@ -7,11 +7,25 @@ from ._processing import Processing as processing
 
 
 class IndexCalculator:
+    """Class for calculating climate indices with xclim."""
+
     def __init__(self, output=True, **kwargs):
+        """Write input parameters to self."""
         self.output = output
         self.kwargs = kwargs
 
     def compute(self, write=False):
+        """Compute climate index.
+
+        Parameters
+        ----------
+        write: bool, optional
+           If True write climate index dataset on disk.
+
+        Returns
+        -------
+        PostProcessing object
+        """
         preproc_obj = preprocessing(**self.kwargs)
         proc_obj = processing(preproc_obj=preproc_obj)
         postproc_obj = postprocessing(proc_obj=proc_obj)
