2cf8d22d052f34b08b30b8d446ac7862b4e6e2c8
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-07-15 09:50:39 +0200

    pr test data

--- index_calculator/_data.py
+++ index_calculator/_data.py
@@ -2,5 +2,11 @@ from pathlib import Path
 
 data_path = Path(__file__).parent
 
-nclist = list((data_path / "data").glob("*"))
-netcdf = [nc.as_posix() for nc in nclist][0]
+nclist_tas = list((data_path / "data").glob("tas_*"))
+nclist_pr = list((data_path / "data").glob("pr_*"))
+netcdf_tas = [nc.as_posix() for nc in nclist_tas][0]
+netcdf_pr = [nc.as_posix() for nc in nclist_pr][0]
+netcdf = {
+    "tas": netcdf_tas,
+    "pr": netcdf_pr,
+}
