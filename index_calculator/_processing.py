1416a33017c1d379089f3645b755741ca6702fb8
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2022-07-18 11:22:55 +0200

    thresholg indices

--- index_calculator/_processing.py
+++ index_calculator/_processing.py
@@ -26,62 +26,84 @@ class Processing:
             )
         object_attrs_to_self(preproc_obj, self)
         self.CIname = check_existance({"index": index}, self)
-        idx_object = self.get_idx_name_and_tresh(kwargs)
-        object_attrs_to_self(idx_object, self)
         kwargs_to_self(kwargs, self)
-        self.replacement = self.get_replacement()
-        self.CIname = self.CIname.replace("YY", self.replacement)
+        self._get_idx_name_and_repl()
         self.proc = self.processing()
 
-    def get_replacement(self):
-        if hasattr(self, "thresh"):
-            return str(self.thresh)
-        elif "thresh" in self.parameters.keys():
-            return str(self.parameters["thresh"])
-        return ""
-
-    def get_idx_name_and_tresh(self, kwargs):
+    def _get_numb_name_and_idx_object(self):
         alpha_name = "".join(filter(lambda x: x.isalpha(), self.CIname))
         numb_name = "".join(filter(lambda x: x.isdigit(), self.CIname))
-        if numb_name:
-            if numb_name[0] == "0":
-                number = float("{}.{}".format(numb_name[0], numb_name[1:]))
-            else:
-                number = int(numb_name)
-        else:
-            number = None
         replace_name = re.sub(r"\d+", "YY", self.CIname)
         if hasattr(indices, self.CIname):
             idx_object = getattr(indices, self.CIname)
             self.IDXname = self.CIname
         elif hasattr(indices, alpha_name):
             idx_object = getattr(indices, alpha_name)
-            kwargs["thresh"] = number
             self.IDXname = alpha_name
         elif hasattr(indices, replace_name):
             idx_object = getattr(indices, replace_name)
-            kwargs["thresh"] = number
             self.IDXname = replace_name
-            if hasattr(self, "thresh"):
-                self.thresh = number
         else:
             raise NameError("{} not defined.".format(self.CIname))
-        return idx_object
+        return numb_name, idx_object
 
-    def adjust_params_to_ci(self):
+    def _get_replacement(self, obj, numb_name):
+        for attr in dir(obj):
+            if attr[0] == "_":
+                continue
+            if not isinstance(getattr(obj, attr), int):
+                continue
+            if attr in self.kwargs.keys():
+                return attr, self.kwargs[attr]
+            if numb_name:
+                return attr, numb_name
+            return attr, getattr(obj, attr)
+        return "", ""
+
+    def _get_idx_name_and_repl(self):
+        numb_name, idx_object = self._get_numb_name_and_idx_object()
+        object_attrs_to_self(idx_object, self, overwrite=False)
+        repl_key, repl_value = self._get_replacement(idx_object, numb_name)
+        if isinstance(repl_value, str) and len(repl_value) > 0:
+            if repl_value[0] == "0":
+                repl_value = float(
+                    "{}.{}".format(
+                        repl_value[0],
+                        repl_value[1:],
+                    )
+                )
+            else:
+                repl_value = int(repl_value)
+
+        if repl_value:
+            self.repl_value = str(repl_value)
+            self.replacement = {repl_key: repl_value}
+        else:
+            self.repl_value = ""
+            self.replacement = {}
+
+        if not self.repl_value:
+            pass
+        elif not numb_name:
+            pass
+        elif "YY" in self.CIname:
+            self.CIname = self.CIname.replace("YY", self.repl_value)
+        elif self.repl_value not in self.CIname:
+            self.CIname = "{}{}".format(self.CIname, self.repl_value)
+        elif numb_name in self.CIname:
+            self.CIname = self.CIname.replace(numb_name, self.repl_value)
+
+    def _adjust_params_to_ci(self):
         params = {
             "ds": self.preproc,
             "freq": _freq[self.freq],
         }
-        params.update(self.parameters)
-        for key, value in self.kwargs.items():
-            if key in self.parameters.keys():
-                params[key] = value
+        params.update(self.replacement)
         return params
 
     def processing(self):
         """Calculate climate index."""
-        params = self.adjust_params_to_ci()
+        params = self._adjust_params_to_ci()
         array = self.compute(**params)
         basics = pyh.basics()
         date_range = basics.date_range(
