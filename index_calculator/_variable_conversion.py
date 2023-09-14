380fa442e167778f7457e457c0f308e40e475f83
Ludwig.Lierhammer <ludwig.lierhammer@hereon.de>
2023-09-14 10:26:04 +0200

    select only requested parameters for conversion

--- index_calculator/_variable_conversion.py
+++ index_calculator/_variable_conversion.py
@@ -1,3 +1,5 @@
+import inspect
+
 import xclim as xc
 
 
@@ -6,10 +8,15 @@ class ConvertVariables:
         self.ds = ds
         self.params = params
 
+    def _params(self, func, params):
+        fparams = inspect.signature(func).parameters
+        return {k: v for k, v in params.items() if k in fparams.keys()}
+
     def sfcWind(self):
         return xc.atmos.wind_speed_from_vector(
             ds=self.ds,
         )[0]
 
     def snd(self):
-        return xc.land.snw_to_snd(ds=self.ds, **self.params)
+        params = self._params(xc.land.snw_to_snd, self.params)
+        return xc.land.snw_to_snd(ds=self.ds, **params)
