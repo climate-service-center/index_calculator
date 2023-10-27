41ed0c02108ef89134a500216ea89d1a1713007b
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2023-10-27 10:52:38 +0200

    bump version

--- index_calculator/__init__.py
+++ index_calculator/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ludwig Lierhammer"""
 __email__ = "ludwig.lierhammer@hereon.de"
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 
 from ._outputwriter import OutputWriter as outputwriter
 from ._postprocessing import PostProcessing as postprocessing
