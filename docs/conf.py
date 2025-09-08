35de691d246666a30d7fb206703a87cc12083ba1
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2025-09-08 17:14:51 +0200

    update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ extensions = [
     #    "sphinxcontrib.mockautodoc",
     "numpydoc",
     "nbsphinx",
-    "nbsphinx_link",
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
 ]
